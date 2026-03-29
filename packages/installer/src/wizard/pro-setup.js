Experiasolutions/KAIROX

fix: resolve pro content from bundled dir instead of npm install (#561)
stepInstallScaffold was trying `npm install @aiox-fullstack/pro` which
doesn't exist on npm. The pro content is already bundled in aiox-core's
`pro/` submodule. Now resolves proSourceDir from the bundled location
first, falling back to node_modules/@aiox-fullstack/pro for legacy.

Co-authored-by: Claude Opus 4.6 <noreply@anthropic.com>

packages/installer/src/wizard/pro-setup.js
@@ -1190,50 +1190,23 @@ async function stepInstallScaffold(targetDir, options = {}) {
 
   const path = require('path');
   const fs = require('fs');
-  const { execSync } = require('child_process');
 
-  const proSourceDir = path.join(targetDir, 'node_modules', '@aiox-fullstack', 'pro');
+  // Resolve pro source directory from multiple locations:
+  // 1. Bundled in aiox-core package (pro/ submodule — npx and local dev)
+  // 2. @aiox-fullstack/pro in node_modules (legacy brownfield)
+  const bundledProDir = path.resolve(__dirname, '..', '..', '..', '..', 'pro');
+  const npmProDir = path.join(targetDir, 'node_modules', '@aiox-fullstack', 'pro');
 
-  // Step 2a: Ensure package.json exists (greenfield projects)
-  const packageJsonPath = path.join(targetDir, 'package.json');
-  if (!fs.existsSync(packageJsonPath)) {
-    const initSpinner = createSpinner(t('proInitPackageJson'));
-    initSpinner.start();
-    try {
-      execSync('npm init -y', { cwd: targetDir, stdio: 'pipe' });
-      initSpinner.succeed(t('proPackageJsonCreated'));
-    } catch (err) {
-      initSpinner.fail(t('proPackageJsonFailed'));
-      return { success: false, error: tf('proNpmInitFailed', { message: err.message }) };
-    }
-  }
-
-  // Step 2b: Install @aiox-fullstack/pro if not present
-  if (!fs.existsSync(proSourceDir)) {
-    const installSpinner = createSpinner(t('proInstallingPackage'));
-    installSpinner.start();
-    try {
-      execSync('npm install @aiox-fullstack/pro', {
-        cwd: targetDir,
-        stdio: 'pipe',
-        timeout: 120000,
-      });
-      installSpinner.succeed(t('proPackageInstalled'));
-    } catch (err) {
-      installSpinner.fail(t('proPackageInstallFailed'));
-      return {
-        success: false,
-        error: tf('proNpmInstallFailed', { message: err.message }),
-      };
-    }
-
-    // Validate installation
-    if (!fs.existsSync(proSourceDir)) {
-      return {
-        success: false,
-        error: t('proPackageNotFound'),
-      };
-    }
+  let proSourceDir;
+  if (fs.existsSync(bundledProDir) && fs.existsSync(path.join(bundledProDir, 'squads'))) {
+    proSourceDir = bundledProDir;
+  } else if (fs.existsSync(npmProDir)) {
+    proSourceDir = npmProDir;
+  } else {
+    return {
+      success: false,
+      error: t('proPackageNotFound'),
+    };
   }
 
   // Step 2c: Scaffold pro content

backstagegrow/aios-core

Cherry-picked from SynkraAI/aiox-core d86ddc5d — keeping aios-core identity.
- Look for bundled pro/ submodule first, fallback to node_modules
Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

packages/installer/src/wizard/pro-setup.js
@@ -1190,52 +1190,23 @@ async function stepInstallScaffold(targetDir, options = {}) {
 
   const path = require('path');
   const fs = require('fs');
-  const proSourceDir = path.join(targetDir, 'node_modules', '@aios-fullstack', 'pro');
 
-  // Step 2a: Ensure package.json exists (greenfield projects)
-  const packageJsonPath = path.join(targetDir, 'package.json');
-  if (!fs.existsSync(packageJsonPath)) {
-    const initSpinner = createSpinner(t('proInitPackageJson'));
-    initSpinner.start();
-    try {
-      const { execFileSync } = require('child_process');
-      const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm';
-      execFileSync(npmCmd, ['init', '-y'], { cwd: targetDir, stdio: 'pipe' });
-      initSpinner.succeed(t('proPackageJsonCreated'));
-    } catch (err) {
-      initSpinner.fail(t('proPackageJsonFailed'));
-      return { success: false, error: tf('proNpmInitFailed', { message: err.message }) };
-    }
-  }
+  // Resolve pro source directory from multiple locations:
+  // 1. Bundled in aios-core package (pro/ submodule — npx and local dev)
+  // 2. @aios-fullstack/pro in node_modules (legacy brownfield)
+  const bundledProDir = path.resolve(__dirname, '..', '..', '..', '..', 'pro');
+  const npmProDir = path.join(targetDir, 'node_modules', '@aios-fullstack', 'pro');
 
-  // Step 2b: Install @aios-fullstack/pro if not present
-  if (!fs.existsSync(proSourceDir)) {
-    const installSpinner = createSpinner(t('proInstallingPackage'));
-    installSpinner.start();
-    try {
-      const { execFileSync } = require('child_process');
-      const npmCmd = process.platform === 'win32' ? 'npm.cmd' : 'npm';
-      execFileSync(npmCmd, ['install', '@aios-fullstack/pro'], {
-        cwd: targetDir,
-        stdio: 'pipe',
-        timeout: 120000,
-      });
-      installSpinner.succeed(t('proPackageInstalled'));
-    } catch (err) {
-      installSpinner.fail(t('proPackageInstallFailed'));
-      return {
-        success: false,
-        error: tf('proNpmInstallFailed', { message: err.message }),
-      };
-    }
-
-    // Validate installation
-    if (!fs.existsSync(proSourceDir)) {
-      return {
-        success: false,
-        error: t('proPackageNotFound'),
-      };
-    }
+  let proSourceDir;
+  if (fs.existsSync(bundledProDir) && fs.existsSync(path.join(bundledProDir, 'squads'))) {
+    proSourceDir = bundledProDir;
+  } else if (fs.existsSync(npmProDir)) {
+    proSourceDir = npmProDir;
+  } else {
+    return {
+      success: false,
+      error: t('proPackageNotFound'),
+    };
   }
 
   // Step 2c: Scaffold pro content